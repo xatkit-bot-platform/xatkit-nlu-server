93507c5a7354556ca78bc473e5df723b44f2d1f8
jordicabot <jcabotsagrera@gmail.com>
2022-03-07 18:00:04 +0100

    Switch configuration properties to instance attributes

--- xatkitnlu/core/nlp_configuration.py
+++ xatkitnlu/core/nlp_configuration.py
@@ -1,26 +1,16 @@
 
 
 class NlpConfiguration:
-    country: str
-    region: str
-    num_words: int # max num of words to keep in the index of words
-    num_epochs: int
-    lower: bool # transform sentences to lowercase
-    oov_token: str #token for the out of vocabulary words
-    embedding_dim: int
-    input_max_num_tokens: int # max length for the vector representing a sentence
-    stemmer: bool #whether to use a stemmer
-
 
     def __init__(self, country: str = "en", region: str = "US", numwords: int = 1000, lower: bool = True, oov_token="<OOV>",
                  num_epochs: int = 300, embedding_dim: int = 16, input_max_num_tokens: int = 30, stemmer: bool = True):
         self.country = country
         self.region = region
-        self.num_words = numwords
-        self.lower = lower
-        self.oov_token = oov_token
+        self.num_words = numwords # max num of words to keep in the index of words
+        self.lower = lower # transform sentences to lowercase
+        self.oov_token = oov_token #token for the out of vocabulary words
         self.num_epochs = num_epochs
         self.embedding_dim = embedding_dim
-        self.input_max_num_tokens = input_max_num_tokens
-        self.stemmer = stemmer
+        self.input_max_num_tokens = input_max_num_tokens # max length for the vector representing a sentence
+        self.stemmer = stemmer #whether to use a stemmer
 
